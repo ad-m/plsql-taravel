25cb1bed2fba94aba73232fb586807e05b228b47
Adam Dobrawy <naczelnik@jawnosc.tk>
2017-06-15 05:32:19 +0200

    Add drop country

--- src/views_country.sql
+++ src/views_country.sql
@@ -10,6 +10,8 @@ PROCEDURE add_country_form;
 PROCEDURE add_country_sql (name_v varchar2, continent_v number);
 PROCEDURE update_country_form(id_v number);
 PROCEDURE update_country_sql (id_v number, name_v varchar2, continent_v number);
+PROCEDURE delete_country_form(id_v number);
+PROCEDURE delete_country_sql(id_v number);
 PROCEDURE form_select_continent(id varchar2, label varchar2, name varchar2, selected varchar2);
 END ADAM_COUNTRY;
 /
@@ -31,20 +33,30 @@ END show_countrys;
 PROCEDURE show_country(id_v number) IS 
     country_v country%ROWTYPE;
     location_count NUMBER;
+    continent_name VARCHAR2(25);
     BEGIN 
     ADAM_GUI.header('ADAM_COUNTRY');
     BEGIN
         SELECT * INTO country_v FROM country WHERE id=id_v;
+        SELECT DECODE(country_v.continent, 1, 'Europa',
+                                              2, 'Azja',
+                                                 'inne') INTO continent_name FROM country WHERE id=id_v;
         SELECT COUNT(id) INTO location_count FROM location WHERE country_id=id_v;
-        ADAM_GUI.button('ADAM_COUNTRY.update_country_form?id_v=' || id_v, 'Zaktualizuj kraj');
+        ADAM_GUI.button_group('ADAM_COUNTRY.update_country_form?id_v=' || id_v, 'Aktualizuj',
+                              'ADAM_COUNTRY.delete_country_form?id_v=' || id_v, 'Usuń');
+        htp.print('<h1>' || country_v.name || '</h1>');
         htp.tableOpen('class="table"');
-        ADAM_GUI.two_column('Kraj', country_v.name);
-        -- , DECODE(country_v.continent, 1, 'Europa',
-        --                                       2, 'Azja',
-        --                                          'inne') AS continent_name
-        -- ADAM_GUI.two_column('Kontynent', country_v.continent_name);
+        ADAM_GUI.two_column('Kontynent', continent_name);
         ADAM_GUI.two_column('Liczba lokalizacji', location_count);
         htp.tableClose;
+        IF location_count > 0 THEN
+            htp.print('<h2>Lokalizacje dla kraju</h2>');
+            htp.print('<ul  class="nav flex-column">');
+            FOR dane IN (SELECT * FROM location WHERE country_id = id_v) LOOP
+                htp.print('<li  class="nav-item"><a  class="nav-link" href="' || ADAM_GUI.url('ADAM_LOCATION.show_location?id_v=' || dane.id ) || '">' || dane.name || '</a></li>');
+            END LOOP;
+            htp.print('</ul>');
+        END IF; 
     END;
     ADAM_GUI.footer;
 END show_country; 
@@ -122,7 +134,7 @@ PROCEDURE update_country_form(id_v number) IS
             when DUP_VAL_ON_INDEX then
                 ADAM_GUI.danger('Oh no!', 'Wprowadzone dane nie sa unikalne');
             when others then
-                ADAM_GUI.danger('Oh no!', 'Wystapil blad');
+                ADAM_GUI.danger(SQLCODE, sqlerrm);
     END;
     ADAM_GUI.footer;
 END update_country_form; 
@@ -146,10 +158,58 @@ PROCEDURE update_country_sql (id_v number, name_v varchar2, continent_v number) 
             when DUP_VAL_ON_INDEX then
                 ADAM_GUI.danger('Oh no!', 'Wprowadzone dane nie sa unikalne');
             when others then
-                ADAM_GUI.danger('Oh no!', 'Wystapil blad');
+                ADAM_GUI.danger(SQLCODE, sqlerrm);
     END;
     ADAM_GUI.footer;
 END update_country_sql;
+
+PROCEDURE delete_country_form(id_v number) IS 
+    country_name country.name%TYPE;
+BEGIN 
+    ADAM_GUI.header('ADAM_COUNTRY');
+    BEGIN
+        SELECT name INTO country_name FROM country WHERE id = id_v;
+        ADAM_GUI.warning('Uwaga!', 'Czy usunac kraj "' || country_name || '"?');
+        ADAM_GUI.button_group('ADAM_COUNTRY.delete_country_sql?id_v=' || id_v, 'Usuń',
+                              'ADAM_COUNTRY.show_country?id_v=' || id_v, 'Anuluj');
+        EXCEPTION
+            when NO_DATA_FOUND then
+                ADAM_GUI.danger('Oh no!', 'Nie znaleziono danych');
+    END;
+    ADAM_GUI.footer;
+END delete_country_form;
+
+PROCEDURE delete_country_sql(id_v number) IS 
+    my_integration_error EXCEPTION;
+    PRAGMA EXCEPTION_INIT (my_integration_error, -20101);
+    count_location number;
+    country_name country.name%TYPE;
+BEGIN
+    ADAM_GUI.header('ADAM_COUNTRY');
+    BEGIN
+        SELECT COUNT(id) INTO count_location FROM location WHERE country_id = id_v;
+        IF count_location > 0 THEN
+            raise_application_error(-20101, 'Naruszenie integralności');
+        END IF;
+        SELECT name INTO country_name FROM country WHERE id = id_v;
+        DELETE FROM country WHERE id = id_v;
+        ADAM_GUI.success('Well done!', 'Pomyslnie usunieto kraj ' || country_name || '.');
+        EXCEPTION
+            when my_integration_error then
+                ADAM_GUI.danger('Oh no!', 'Naruszenie integralności');
+            when NO_DATA_FOUND then
+                ADAM_GUI.danger('Oh no!', 'Nie znaleziono danych');
+            WHEN STORAGE_ERROR THEN
+                ADAM_GUI.danger('Oh no!', 'Blad pamieci');
+            when INVALID_NUMBER then
+                ADAM_GUI.danger('Oh no!', 'Wprowadzono niepoprawna wartosc'); 
+            when VALUE_ERROR then
+                ADAM_GUI.danger('Oh no!', 'Blad konwersji typów danych');
+            when others then
+                ADAM_GUI.danger(SQLCODE, sqlerrm);
+    END;
+    ADAM_GUI.footer;
+END delete_country_sql;
 
 PROCEDURE form_select_continent(id varchar2, label varchar2, name varchar2, selected varchar2) IS
 BEGIN
